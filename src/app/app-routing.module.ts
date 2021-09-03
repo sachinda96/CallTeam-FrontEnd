751d6724139015274b4c905a9abb5a510c102d8f
sachinda <sachinda9696@gmail.com>
2021-09-04 01:48:58 +0530

    Complete user dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import {LoginComponent} from "./view/login/login.component";
+import {NavComponent} from "./view/nav/nav.component";
+import {HomeComponent} from "./view/home/home.component";
+import {NavDashboardComponent} from "./view/nav-dashboard/nav-dashboard.component";
+import {DashboardComponent} from "./view/dashboard/dashboard.component";
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', component: NavComponent,
+  children: [
+    {
+      path: '',
+      component: HomeComponent
+    },
+    {
+      path: 'login',
+      component: LoginComponent
+    },
+    {
+      path: 'dashboard',
+      component: NavDashboardComponent,
+      children:[
+        {
+          path: '',
+          component: DashboardComponent
+        }
+      ]
+    }
+
+
+  ]},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './view/login/login.component';
+import { HomeComponent } from './view/home/home.component';
+import { NavComponent } from './view/nav/nav.component';
+import {CarouselModule} from "ngx-owl-carousel-o";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { NavDashboardComponent } from './view/nav-dashboard/nav-dashboard.component';
+import {DashboardComponent} from "./view/dashboard/dashboard.component";
+import {DatePipe} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    LoginComponent,
+    HomeComponent,
+    NavComponent,
+    DashboardComponent,
+    NavDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
